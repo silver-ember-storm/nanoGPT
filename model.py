84eb813cd6a3b43e0295d5a2ab616ea646d90317
silver-ember-storm <chaidan@ucsb.edu>
2025-10-02 19:36:31 -0700

    Update model.py

--- model.py
+++ model.py
@@ -24,11 +24,11 @@ class CausalSelfAttention(nn.Module):
         self.key_query_dim = config.n_embd // config.n_head
         self.value_dim = config.n_embd // config.n_head
         # key, query, value projections for all heads, but in a batch
-        self.gen_key = nn.Linear(config.n_embd, self.key_query_dim)
-        self.gen_query = nn.Linear(config.n_embd, self.key_query_dim)
-        self.gen_value = nn.Linear(config.n_embd, self.value_dim)
+        self.gen_key = nn.Linear(config.n_embd, self.key_query_dim * config.n_head)
+        self.gen_query = nn.Linear(config.n_embd, self.key_query_dim * config.n_head)
+        self.gen_value = nn.Linear(config.n_embd, self.value_dim * config.n_head)
         # output projection
-        self.output = nn.Linear(self.value_dim, config.n_embd)
+        self.output = nn.Linear(self.value_dim * config.n_head, config.n_embd)
         # regularization
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
